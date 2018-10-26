c594ea888eec7af6eade550a9ed18da7fdf6f5c4
fedepvg <federicopvg@gmail.com>
2018-10-26 12:05:34 -0300

    modificado el gitignore

--- gradius/src/pantallas/pantallaMenu.cpp
+++ gradius/src/pantallas/pantallaMenu.cpp
@@ -23,7 +23,7 @@ namespace Juego {
 			double tamanioTitulo = (GetScreenHeight()*GetScreenWidth())*0.0190 / 100;
 			double tamanioVersion = (GetScreenHeight()*GetScreenWidth())*0.0042 / 100.0;
 			char titulo[] = "Gradius";
-			char version[] = "Gradius v0.2";
+			char version[] = "Gradius v0.3";
 			dibujarBotones();
 			DrawText(titulo, (GetScreenWidth() - MeasureText(titulo, tamanioTitulo)) / 2, GetScreenHeight()*0.1f, tamanioTitulo, DARKGRAY);
 			DrawText(version, GetScreenWidth() - MeasureText(version, tamanioVersion), 0 + tamanioVersion, (int)tamanioVersion, DARKGRAY);
